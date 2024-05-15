b01fb0ece5f11a3136b7627bc7be2a2076db1236
ellafsd <ellagirin82@gmail.com>
2024-05-15 11:26:41 -0400

    test file for plants

--- seeds/seedPlants.js
+++ seeds/seedPlants.js
@@ -1,18 +1,29 @@
 const fs = require('fs');
-const path = require('path');    //imports the Node.js path
-const plantsFilePath = path.join(__dirname, 'plants.json');   // Path to plants.json
-
+const path = require('path'); //imports the Node.js path
+const plantsFilePath = path.join(__dirname, 'plants.json'); // Path to plants.json
+const axios = require('axios');
 // Function to read JSON file
 const readJSONFile = (filePath) => {
   //return new Promise((resolve, reject) => {
-    fs.readFile("./plants.json", 'utf8', (err, data) => {
-      if (err) {
-        reject(err);
-      } else {
-        console.log(JSON.parse(data));
-        return JSON.parse(data);
-      }
-    });
+  fs.readFile('./plants.json', 'utf8', (err, data) => {
+    if (err) {
+      reject(err);
+    } else {
+      let plantsData = JSON.parse(data);
+      const url = `https://perenual.com/api/species/details/${plantsData[0].id}?key=sk-4J5W664232c727ead5452`;
+      
+      axios
+        .get(url)
+        .then((response) => {
+          console.log(JSON.stringify(response.data));
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+      //console.log(JSON.parse(data));
+      return JSON.parse(data);
+    }
+  });
   //});
 };
 
@@ -20,20 +31,9 @@ const seedPlants = async () => {
   try {
     // Read data from plants.json
     const plantsData = await readJSONFile(plantsFilePath);
-
-    // Sync the database
-    const seedPlants = async () => {
-      try {
-          const plantsData = await readJSONFile(plantsFilePath);
-          console.log(plantsData); // This will print the parsed JSON data to the console
-          // Add your database seeding logic here
-      } catch (err) {
-          console.error(err);
-      }
-};
-} catch (err) {
-  console.error(err);
-}
+  } catch (err) {
+    console.error(err);
+  }
 };
 
 // Execute the seeding function
