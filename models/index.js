0b29f3baa3f92dd7ed022661829733353446ba2a
Dylan Thrasher <thrasher_dylan@yahoo.com>
2024-05-14 20:34:27 -0400

    removed instances of Project in index.js

--- models/index.js
+++ models/index.js
@@ -1,15 +1,6 @@
 const User = require('./User');
-const Project = require('./Project');
 const Plant = require('./Plant');
 
-User.hasMany(Project, {
-  foreignKey: 'user_id',
-  onDelete: 'CASCADE'
-});
-
-Project.belongsTo(User, {
-  foreignKey: 'user_id'
-});
 
 User.hasMany(Plant, {
   foreignKey: 'user_id',
@@ -20,4 +11,4 @@ Plant.belongsTo(User, {
   foreignKey: 'user_id'
 });
 
-module.exports = { User, Project, Plant };
+module.exports = { User, Plant };
