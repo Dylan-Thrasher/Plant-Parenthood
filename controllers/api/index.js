dcd8413e72c1976700c8223c911228e3460f81c3
Dylan Thrasher <thrasher_dylan@yahoo.com>
2024-05-16 16:07:07 -0400

    updated R&D folder to contain testing information.

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const plantRoutes = require('./plantRoutes');
+const collectionRoutes = require('./collectionRoutes')
 
 router.use('/users', userRoutes);
 router.use('/plants', plantRoutes);
+router.use('/collection', collectionRoutes);
 
 module.exports = router;
