77ca3ada94de7d506668e034b9f1834fbc8ffbca
frenzie24 <kevincharlesgross@gmail.com>
2024-05-19 17:46:19 -0400

    sorting implmemented with new styling on homepage

--- public/js/imageGrid.js
+++ public/js/imageGrid.js
@@ -27,6 +27,39 @@ const handleEditClick = (ev => {
     window.location.href = `/collection?id=${id}&edit=true`;
 })
 
+
+const handleZASort = (ev) => {
+    ev.preventDefault();
+    
+    $('[id=sortZA').prop('checked', true);
+    $('[id=sortAZ').prop('checked', false);
+    sortZA();
+}
+
+const sortZA = () => {
+    let grid = $('#resultsGrid');
+    let entries = grid.children();
+    entries.sort(function (a, b) {
+        var nameA = a.attributes[1].value.toLowerCase(), nameB = b.attributes[1].value.toLowerCase();
+        if (nameA > nameB) //sort string ascending
+            return -1;
+        if (nameA < nameB)
+            return 1;
+        return 0; //default return value (no sorting)
+    });
+    grid.empty();
+    grid.append(entries);
+    console.log(entries);
+}
+
+const handleAZSort = (ev) => {
+    ev.preventDefault();
+    
+    $('[id=sortAZ').prop('checked', true);
+    $('[id=sortZA').prop('checked', false);
+    sortAZ();
+}
+
 const sortAZ = () => {
     debugger;
     let grid = $('#resultsGrid');
@@ -48,4 +81,6 @@ $(() => {
     $('[id=plantImg]').on('click', handleImageClick);
     $('[id=collectionImg]').on('click', handleCollectionImageClick);
     $('[id=edit-btn]').on('click', handleEditClick);
+    $('[id=sortAZ]').on('click', handleAZSort);
+    $('[id=sortZA]').on('click', handleZASort);
 })
