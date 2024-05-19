74793a8a28c20800aebd24dc8d40f6041b4965d5
frenzie24 <kevincharlesgross@gmail.com>
2024-05-19 17:19:59 -0400

    wrote initial logic for sorting

--- public/js/imageGrid.js
+++ public/js/imageGrid.js
@@ -6,17 +6,17 @@ const handleImageClick = (ev) => {
     let id = ev.target.name;
 
     window.location.href = `/plant?id=${id}`;
-   
+
 }
 
-const handleCollectionClick = (ev) => {
+const handleCollectionImageClick = (ev) => {
     ev.preventDefault();
-    
+
     // we use the name html attribute in profile view to track the collection id
     let id = ev.target.name;
-    
+
     window.location.href = `/collection?id=${id}`;
-   
+
 }
 
 // Handles clicking the edit button for a plant on the users plant grid on the profile view
@@ -27,9 +27,25 @@ const handleEditClick = (ev => {
     window.location.href = `/collection?id=${id}&edit=true`;
 })
 
-$(()=>{
-    
-    $('[id=plantCard]').on('click', handleImageClick);
-    $('[id=collectionCard]').on('click', handleCollectionClick);
-    $('[id=edit-btn]').on('click', handleEditClick); 
+const sortAZ = () => {
+    debugger;
+    let grid = $('#resultsGrid');
+    let entries = grid.children();
+    entries.sort(function (a, b) {
+        var nameA = a.attributes[1].value.toLowerCase(), nameB = b.attributes[1].value.toLowerCase();
+        if (nameA < nameB) //sort string ascending
+            return -1;
+        if (nameA > nameB)
+            return 1;
+        return 0; //default return value (no sorting)
+    });
+    grid.empty();
+    grid.append(entries);
+    console.log(entries);
+}
+$(() => {
+
+    $('[id=plantImg]').on('click', handleImageClick);
+    $('[id=collectionImg]').on('click', handleCollectionImageClick);
+    $('[id=edit-btn]').on('click', handleEditClick);
 })
