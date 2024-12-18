84cae83ac40f81d72edcc99469c1746f1e4f7e78
frenzie24 <kevincharlesgross@gmail.com>
2024-12-18 17:31:50 -0600

    removed collection generation from seed

--- seeds/seed.js
+++ seeds/seed.js
@@ -4,35 +4,39 @@ const { User, Plant, Collection } = require('../models');
 const userData = require('./userData.json');
 const plantData = require('./data.json');
 const collectionData = require('./collection.json')
-const {log, info, warn, error} = require('@frenzie24/logger')
+const { log, info, warn, error } = require('@frenzie24/logger')
 
 // must be run in order to create users and register information from userData.json
 const seedDatabase = async () => {
-  await sequelize.sync({ force: true });
+  try {
+    await sequelize.sync({ force: true });
 
-  const users = await User.bulkCreate({userData}, {
-    individualHooks: true,
-    returning: true,
-  });
- // console.log('plantData:', plantData);
-  for (const plant of plantData) {
-    info(['plant: ',plant])
+    const users = await User.bulkCreate({ userData }, {
+      individualHooks: true,
+      returning: true,
+    });
+
+  } catch (e) { warn('Caught Error'); error(e); }
+  // console.log('plantData:', plantData);
+  try {
+  } catch (e) { warn('Caught Error'); error(e); } for (const plant of plantData) {
+    info(['plant: ', plant])
     await Plant.create({
       ...plant,
       user_id: plant.user_id,
     });
-  }
-  console.log('collectionData:', collectionData);
-
-  //setTimeout(()=>{
-  for (const collection of collectionData) {
-    Collection.create({
+  } try {
+  } catch (e) { warn('Caught Error'); error(e); }
+ /* console.log('collectionData:', collectionData);
+  try {
+  } catch (e) { warn('Caught Error'); error(e); } for (const collection of collectionData) {
+    await Collection.create({
       ...collection,
-     // user_id: collection.user_id,
+    //  user_id: collection.user_id,
     });
-  }
-  //}}, 2000);
-
+  } try {
+  } catch (e) { warn('Caught Error'); error(e); }
+*/
   process.exit(0);
 };
 
